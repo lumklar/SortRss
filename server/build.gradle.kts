848a286c3353667007d60b01cb883ea45440af4e
lumklar <lumklar@outlook.com>
2026-06-01 17:53:05 +0800

    引入jpa

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     kotlin("jvm") version "2.3.21"
     kotlin("plugin.spring") version "2.3.21"
-//    id("java")
     id("org.springframework.boot") version "4.1.0-RC1"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.graalvm.buildtools.native") version "0.11.5"
@@ -17,6 +16,9 @@ tasks.bootJar {
 }
 
 graalvmNative {
+    metadataRepository {
+        enabled = true
+    }
     binaries {
         named("main") {
             buildArgs.add("-O2")
@@ -27,7 +29,25 @@ graalvmNative {
 
 dependencies {
     implementation(project(":common"))
+
+    //web
     implementation("org.springframework.boot:spring-boot-starter-web")
+    //缓存
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+    implementation("com.github.ben-manes.caffeine:caffeine")
+    //数据库
+    implementation("jakarta.persistence:jakarta.persistence-api")
+//    implementation("com.baomidou:mybatis-plus-spring-boot4-starter:3.5.16")
+//    implementation("com.baomidou:mybatis-plus-jsqlparser:3.5.16")
+//    implementation("org.mybatis.spring.boot:mybatis-spring-boot-starter:4.0.1")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    //lombok
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+    // 测试依赖
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
